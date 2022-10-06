055551cb068b800b78f7855a4597ab8c9ba84d2a
“SenneDmn” <senne.deman@student.ehb.begit config --global user.name SenneDmn git config --global user.email senne.deman@student.ehb.be>
2022-10-06 12:08:57 +0200

    Oefening 3

---
+++ exercises/1-functions/1-3-rectangles.js
@@ -0,0 +1,53 @@
+"use strict";
+
+let canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let context = canvas.getContext('2d');
+
+drawRectangles();
+
+function drawRectangles() {
+//vierkant linksboven  
+    context.strokeStyle = "red"
+    context.beginPath();
+    context.rect(50, 50, 150, 150);
+    context.stroke();
+
+//vierkant rechtsonder 
+    context.strokeStyle = "red"
+    context.beginPath();
+    context.rect(200, 200, 150, 150);
+    context.stroke();
+
+//vierkant midden  
+    context.strokeStyle = "red"
+    context.beginPath();
+    context.rect(125, 125, 150, 150);
+    context.stroke();
+    context.fill();
+
+//rechtsboven_stroke 
+    context.strokeStyle = "red"
+    context.beginPath();
+    context.rect(275, 75, 50, 50);
+    context.stroke();
+
+//rechtsboven_fill 
+    context.strokeStyle = "red"
+    context.beginPath();
+    context.rect(325, 50, 25, 25);
+    context.fill();
+
+//linksonder_stroke 
+    context.strokeStyle = "red"
+    context.beginPath();
+    context.rect(75, 275, 50, 50);
+    context.stroke();
+
+//linksonder_fill 
+    context.strokeStyle = "red"
+    context.beginPath();
+    context.rect(50, 325, 25, 25);
+    context.fill(); 
+}
