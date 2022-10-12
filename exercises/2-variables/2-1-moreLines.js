3506feea0bf334faeaf7523d17792136b3ac63dd
“SenneDmn” <senne.deman@student.ehb.begit config --global user.name SenneDmn git config --global user.email senne.deman@student.ehb.be>
2022-10-12 10:18:50 +0200

    Oefening 2.1

---
+++ exercises/2-variables/2-1-moreLines.js
@@ -0,0 +1,27 @@
+"use strict";
+
+let canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let context = canvas.getContext('2d');
+
+let margin = 100;
+let width = context.canvas.width;
+
+drawLines();
+
+function drawLines() { 
+    context.beginPath();
+    context.moveTo(margin,100);
+    context.lineTo(width-margin,100);
+    context.lineTo(margin,200);
+    context.lineTo(width-margin,200);
+    context.lineTo(margin,300);
+    context.lineTo(width-margin,300);
+    context.lineTo(margin,400);
+    context.lineTo(width-margin,400);
+    context.lineTo(margin,500);
+    context.lineTo(width-margin,500);
+    context.lineTo(margin, 100);
+    context.stroke();
+}
