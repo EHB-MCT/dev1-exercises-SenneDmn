609052b126d822e0ab011ba78983661e070fb178
“SenneDmn” <senne.deman@student.ehb.begit config --global user.name SenneDmn git config --global user.email senne.deman@student.ehb.be>
2022-10-26 10:28:42 +0200

    Oefening 3.1.a

--- exercises/3-iteration-while/3-1-a.js
+++ exercises/3-iteration-while/3-1-a.js
@@ -2,24 +2,24 @@
 import context from "../../scripts/context.js";
 import * as Utils from "../../scripts/utils.js";
 
-/** @type {CanvasRenderingContext2D} */
+let step = 25;
  
 
 draw();
 
 function draw() {
     context.lineWidth = 2;
-    context.strokeStyle = "black";
+    context.strokeStyle = "white";
     context.fillStyle = "#3498DB";
     context.fillRect(50, 50, 300, 300);
     drawLines();
 }
 
 function drawLines() {
-    let i = 1;
+    let i = 0;
     while (i<=11) {
         
-        Utils.drawLine(30, i * 10, 270, i*10);
-        i ++ ;
+        Utils.drawLines(75, 75+step*i, 325, 75+step*i);
+        i += 1;
     }
 }
